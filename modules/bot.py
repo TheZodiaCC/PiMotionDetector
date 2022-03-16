271cb9b7ca38bfa582165116013a4de9dd43fb70
TheZodiaCC <53448802+TheZodiaCC@users.noreply.github.com>
2022-03-16 20:37:18 +0100

    commands system refactoring

--- modules/bot.py
+++ modules/bot.py
@@ -23,25 +23,40 @@ class RPiMotionDetectorBOT(discord.Client):
         log_utils.log_bot(f"Logged on as {self.user}, Guild: {guild}, Target Channel: {target_channel}")
 
     async def on_message(self, message):
-        if message.author != self.user:
-
+        if message.author != self.user and self.get_target_channel().name == AppConfig.DISCORD_TARGET_CHANNEL:
             if message.content[0] == AppConfig.BOT_COMMAND_PREFIX:
-                message_content = message.content.replace("!", "")
 
-                content = "Not Found"
+                await message.channel.send(self.commands_handler(message))
 
-                if message_content == "list":
-                    content = " ".join(os.listdir(AppConfig.LOG_FILES_DIR_PATH))
+    def commands_handler(self, command):
+        command_content = command.content.replace("!", "")
 
-                elif message_content.split(" ")[0] == "get" and len(message_content.split(" ")) == 2:
+        output = "Not Found"
 
-                    file = message_content.split(" ")[1]
-                    log_files = os.listdir(AppConfig.LOG_FILES_DIR_PATH)
+        if command_content == "list":
+            output = self.list_command()
 
-                    if file in log_files:
-                        content = log_utils.read_log_file(file)
+        elif command_content.split(" ")[0] == "get" and len(command_content.split(" ")) == 2:
+            output = self.get_command(command_content)
 
-                elif message_content == "help":
-                    content = "list, get"
+        elif command_content == "help":
+            output = "list\nget\n"
 
-                await message.channel.send(content)
+        return output
+
+    def list_command(self):
+        return " ".join(os.listdir(AppConfig.LOG_FILES_DIR_PATH))
+
+    def get_command(self, command):
+        output = ""
+
+        file = command.split(" ")[1]
+        log_files = os.listdir(AppConfig.LOG_FILES_DIR_PATH)
+
+        if file in log_files:
+            raw_output = log_utils.read_log_file(file)
+            raw_output= "_".join(raw_output).replace("\n", "").split("_")[-50:]
+
+            output = "\n".join(raw_output)
+
+        return output
